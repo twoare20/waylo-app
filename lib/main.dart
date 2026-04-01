d5334aa3b997e0300a50125e6ad652435b7b3a20
twoare20 <twoare20@gmail.com>
2026-04-01 22:36:26 +0900

    Add lib/main.dart

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+```dart
+import 'package:flutter/material.dart';
+import 'package:waylo/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @Override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'WAYLO',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+```
